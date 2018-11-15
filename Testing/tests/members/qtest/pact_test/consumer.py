eef9cc11e9d2f9beab5afe3122c937da630349eb
huiboSong <songbo1220@126.com>
2018-11-15 11:27:34 +0800

    删除重复代码

--- Testing/tests/members/qtest/pact_test/consumer.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def is_up(url):
-    return requests.get(url).status_code == 200
-
-def user(user_name):
-    uri = 'http://localhost:1234/users/' + user_name
-    return requests.get(uri).json()
--- Testing/tests/members/yangyueqing/ZMSY/Login/__init__.py
+++
@@ -1,3 +0,0 @@
-
-__author__ = 'co-mall'
-
